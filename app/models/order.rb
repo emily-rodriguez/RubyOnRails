5f94d2d016ee651648aa44385feb98053ac94d5a
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-08-08 12:05:26 -0500

    added admin and sessions controllers

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,17 +2,16 @@ require 'active_model/serializers/xml'
 require 'pago'
 
 class Order < ApplicationRecord
-  include ActiveModel::Serializers::XML
+  include ActiveModel::Serializers::Xml
   enum pay_type: {
       "Check"          => 0,
-      "Credit Card"    => 1,
-      "Purchase Order" => 2
+      "Credit card"    => 1,
+      "Purchase order" => 2
   }
   has_many :line_items, dependent: :destroy
-
+  # ...
   validates :name, :address, :email, presence: true
   validates :pay_type, inclusion: pay_types.keys
-
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
       item.cart_id = nil
@@ -29,21 +28,21 @@ class Order < ApplicationRecord
       payment_method = :check
       payment_details[:routing] = pay_type_params[:routing_number]
       payment_details[:account] = pay_type_params[:account_number]
-    when "Credit Card"
+    when "Credit card"
       payment_method = :credit_card
       month,year = pay_type_params[:expiration_date].split(//)
       payment_details[:cc_num] = pay_type_params[:credit_card_number]
       payment_details[:expiration_month] = month
       payment_details[:expiration_year] = year
-    when "Purchase Order"
+    when "Purchase order"
       payment_method = :po
       payment_details[:po_num] = pay_type_params[:po_number]
     end
 
     payment_result = Pago.make_payment(
-      order_id: id,
-      payment_method: payment_method,
-      payment_details: payment_details
+        order_id: id,
+        payment_method: payment_method,
+        payment_details: payment_details
     )
 
     if payment_result.succeeded?
